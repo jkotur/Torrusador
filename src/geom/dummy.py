1b1d5c009eca50938e60e4d7c6ee79abea494cd5
Jakub <qba@bender.(none)>
2011-03-04 11:07:41 +0100

    Zmainy laboratoryjen
    + dodana zmiana near clipping
    + reczne mnozenie wektorow

--- src/geom/dummy.py
+++ src/geom/dummy.py
@@ -2,7 +2,9 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-import numpy
+
+import numpy as np
+from numpy.linalg import linalg as la
 
 class Dummy :
 
@@ -23,8 +25,11 @@ class Dummy :
 		self.mode = 0
 
 	def draw( self ) :
+		self.draw_by_hand()
+
+	def draw_with_ogl( self ):
 		if self.mode == 0 :
-			self.geom = numpy.array(self.geometry())
+			self.geom = np.array(self.geometry())
 			self.count = len(self.geom)/3
 
 #            glBindBuffer(GL_ARRAY_BUFFER,self.bid)
@@ -38,4 +43,28 @@ class Dummy :
 		glDrawArrays(GL_LINE_STRIP,0,self.count)
 #        glBindBuffer(GL_ARRAY_BUFFER,0)
 		glDisableClientState(GL_VERTEX_ARRAY)
+
+	def draw_by_hand( self ) :
+		if self.mode == 0 :
+			self.geom = self.geometry()
+			self.geom = [ np.array(self.geom[i:i+3]+[1]) for i in range(0,len(self.geom),3) ]
+			self.count = len(self.geom)
+
+		m = glGetFloatv(GL_TRANSPOSE_MODELVIEW_MATRIX)
+
+		ng = []
+		for v in self.geom :
+			w = la.dot(m,v)
+			w/= w[3]
+			ng += list(w)[0:3]
+
+		ng = [ ng[i:i+3] for i in range(0,len(self.geom),3) if ng[i+2] ]
+
+		glPushMatrix()
+		glLoadIdentity()
+		glEnableClientState(GL_VERTEX_ARRAY)
+		glVertexPointer(3,GL_FLOAT,0,ng)
+		glDrawArrays(GL_LINE_STRIP,0,self.count)
+		glDisableClientState(GL_VERTEX_ARRAY)
+		glPopMatrix()
 
